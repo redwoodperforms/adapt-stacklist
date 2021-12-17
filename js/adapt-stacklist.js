2a9bdffa0305f9a43d720d956b4f2433041c2d8d
robmausser <robmausser@gmail.com>
2021-12-17 15:19:35 -0500

    adapt 5 stacklist update. Auto stack. Move stack into viewport.

--- js/adapt-stacklist.js
+++ js/adapt-stacklist.js
@@ -1,25 +1,25 @@
-define(function(require) {
-
-    var ComponentView = require('coreViews/componentView');
-    var Adapt = require('coreJS/adapt');
+define([
+    'core/js/adapt',
+    'coreViews/componentView'
+], function (Adapt, ComponentView) {
 
     var StackList = ComponentView.extend({
 
         TRANSITION_TIME: 250,
 
         events: {
-            "click .stacklist-next": "nextItem"
+            "click .stacklist__next": "nextItem"
         },
 
-        preRender: function() {
+        preRender: function () {
             this.model.set("_globals", Adapt.course.get("_globals"));
             this.model.set("_stage", -1);
             this.setupButton();
         },
 
-        postRender: function() {
-            this.$items = this.$(".stacklist-item");
-            this.$button = this.$('.stacklist-button');
+        postRender: function () {
+            this.$items = this.$(".stacklist__item");
+            this.$button = this.$('.stacklist__button');
 
             if (!this.model.get("_isComplete") || this.model.get("_isResetOnRevisit")) {
                 this.setupListItems();
@@ -29,7 +29,7 @@ define(function(require) {
             this.setReadyStatus();
         },
 
-        setupButton: function() {
+        setupButton: function () {
             var _button = this.model.get("_button") || {};
             // Set up button aria label
 
@@ -43,12 +43,12 @@ define(function(require) {
             this.model.set("_button", _button);
         },
 
-        setupListItems: function() {
-            this.$items.addClass("visibility-hidden");
+        setupListItems: function () {
+            this.$items.addClass("display-none");
             this.$button.show();
         },
 
-        setupItemOffsets: function() {
+        setupItemOffsets: function () {
             // Set item positions alternating R and L
             var wWin = $(window).width();
             var offsetLeft = -this.$el.outerWidth();
@@ -56,7 +56,7 @@ define(function(require) {
             var stage = this.model.get('_stage');
             var buttonOffset = 0;
 
-            this.$items.each(function(i) {
+            this.$items.each(function (i) {
                 var $el = $(this);
 
                 if (i <= stage) {
@@ -70,31 +70,41 @@ define(function(require) {
                 $el.offset(offset);
             });
 
-            this.$button.css({top: buttonOffset});
+            this.$button.css({ top: buttonOffset });
 
 
         },
 
-        nextItem: function() {
-            var stage = this.model.get("_stage") + 1;
-            this.setStage(stage);
+        nextItem: function () {
+            // var stage = this.model.get("_stage") + 1;
+            // this.setStage(stage);
+
+            var items = this.model.get("_items");
+            this.$(".stacklist__button").remove();
+            _.each(items, function (item, index) {
+                _.delay(function () {
+                    var stage = this.model.get("_stage") + 1;
+                    this.setStage(stage);
+                }.bind(this), 250 * (index + 1));
+
+            }.bind(this));
         },
 
-        setStage: function(stage) {
+        setStage: function (stage) {
             this.model.set("_stage", stage);
 
             var items = this.model.get("_items");
             var isComplete = this.model.get("_items").length - 1 === stage;
-            var $item = this.$(".stacklist-item").eq(stage);
+            var $item = this.$(".stacklist__item").eq(stage);
 
-            $item.removeClass("visibility-hidden");
+            $item.removeClass("display-none");
 
             var h = $item.outerHeight(true);
 
             $item.velocity({ left: 0 }, {
                 delay: this.TRANSITION_TIME,
                 duration: this.TRANSITION_TIME,
-                complete: function() {
+                complete: function () {
                     $item.addClass("show").a11y_focus();
                 }
             });
@@ -103,35 +113,35 @@ define(function(require) {
                 this.onComplete();
                 this.updateButton('', h);
             } else {
-                var continueText = items[stage].next || this.model.get("_button").continueText;
-                var btnAriaLabel = this.model.get("_globals")._components._stacklist.ariaButtonLabel || this.model.get("_globals")._accessibility._ariaLabels.next;
-                var ariaLabel = continueText + ', ' + btnAriaLabel;
+                // var continueText = items[stage].next || this.model.get("_button").continueText;
+                // var btnAriaLabel = this.model.get("_globals")._components._stacklist.ariaButtonLabel || this.model.get("_globals")._accessibility._ariaLabels.next;
+                // var ariaLabel = continueText + ', ' + btnAriaLabel;
 
-                this.updateButton(continueText, h, ariaLabel);
+                // this.updateButton(continueText, h, ariaLabel);
             }
         },
 
-        updateButton: function(text, offset, ariaLabel) {
-            this.$(".stacklist-button").css({ top: "+=" + offset });
+        updateButton: function (text, offset, ariaLabel) {
+            this.$(".stacklist__button").css({ top: "+=" + offset });
 
             if (text === '') { // On last item we do not want to update text (it's most important when stack-list has only one item)
                 return;
             }
 
-            var $button = this.$(".stacklist-next");
+            var $button = this.$(".stacklist__next");
             $button.blur();
-            setTimeout(function() {
+            setTimeout(function () {
                 $button.html(text);
                 $button.attr('aria-label', ariaLabel);
             }, this.TRANSITION_TIME * 2);
         },
 
-        onComplete: function() {
-            var $buttonDiv = this.$(".stacklist-button");
-            var $button = this.$(".stacklist-next");
+        onComplete: function () {
+            var $buttonDiv = this.$(".stacklist__button");
+            var $button = this.$(".stacklist__next");
             $buttonDiv.css({ opacity: 0 });
 
-            setTimeout(function() {
+            setTimeout(function () {
                 $button.remove();
             }, this.TRANSITION_TIME);
 
